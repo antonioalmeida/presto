927337afa90a42185bf77cb03db3f9b5ba32917d
theantonioalmeida <tony-alm@hotmail.com>
2018-03-04 20:50:01 +0000

    Redesign search page - improve mobile UX

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -5,3 +5,7 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
 })
+
+$("#checkAll").change(function () {
+    $(".typeFilter input:checkbox").prop('checked', $(this).prop("checked"));
+});
