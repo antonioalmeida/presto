f9fb9bf765dfc3d4253f96d2a9b98dca658118a3
darksystem <diogo.rey@hotmail.com>
2018-04-07 13:48:59 +0100

    Refactor index page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,12 +6,8 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    private $view;
 
     //
-    public function __construct(){
-        $this->middleware('guest');
-    }
 
     public function show(){
 
