78542afb69d24a59dab2d4361a287f26739ee27e
theantonioalmeida <tony-alm@hotmail.com>
2018-04-13 16:54:16 +0100

    Implement sample dynamic comment view on question page

--- resources/views/partials/answer.blade.php
+++ resources/views/partials/answer.blade.php
@@ -34,70 +34,24 @@
     <div class="card-body">
         <h6><small>3 Comments</small></h6>
         <div class="d-flex list-group list-group-flush short-padding">
-            <div class="list-group-item">
 
-                @include('partials.comment')
+                @foreach ($answer->comments as $comment)
+                    @include('partials.comment', ['comment' => $comment])
+                @endforeach
 
             <div class="collapse" id="collapseExample">
 
-                <div class="list-group-item">
-                    <div class="d-flex flex-column">
-                        <div class="ml-1">
-                            <div class="d-flex">
-                                <div class="align-self-center">
-                                    <img class="rounded-circle mr-2" width="36px" heigth="36px" src="assets/img/portrait-man.jpeg">
-                                </div>
-                                <div class="d-flex flex-column">
-                                    <span><strong>António Almeida</strong></span>
-                                    <span class="text-muted">5h ago</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pl-2 mt-1">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et nibh ac massa tristique semper.  Phasellus eu orci quis erat rhoncus feugiat eget congue leo.</p>
-                            <div class="d-flex justify-content-between">
-                                <div>
-                                    <a class="text-muted" href="">Upvote</a> <span class="text-muted">&bull;</span> <a class="text-muted" href="#">Downvote</a>
-                                </div>
-                                <div class="ml-auto">
-                                    <a href="#" class="text-muted"><small>Report</small></a>
-                                </div>
-                            </div>  </div>
-                        </div>
-                    </div>
-
-                    <div class="list-group-item">
-                        <div class="d-flex flex-column">
-                            <div class="ml-1">
-                                <div class="d-flex">
-                                    <div class="align-self-center">
-                                        <img class="rounded-circle mr-2" width="36px" heigth="36px" src="assets/img/portrait-man.jpeg">
-                                    </div>
-                                    <div class="d-flex flex-column">
-                                        <span><strong>António Almeida</strong></span>
-                                        <span class="text-muted">5h ago</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="pl-2 mt-1">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et nibh ac massa tristique semper.  Phasellus eu orci quis erat rhoncus feugiat eget congue leo.</p>
-                                <div class="d-flex justify-content-between">
-                                    <div>
-                                        <a class="text-muted" href="">Upvote</a> <span class="text-muted">&bull;</span> <a class="text-muted" href="#">Downvote</a>
-                                    </div>
-                                    <div class="ml-auto">
-                                        <a href="#" class="text-muted"><small>Report</small></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <a class="btn btn-lg btn-link text-dark" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                    View More
-                </a>
+                <!-- add extra comments -->
+                @foreach ($answer->comments as $comment)
+                    @include('partials.comment', ['comment' => $comment])
+                @endforeach
 
             </div>
+
+            <a class="btn btn-lg btn-link text-dark" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                View More
+            </a>
+
         </div>
     </div>
+</div>
