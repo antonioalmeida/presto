568f1bf2dbefd2b886e15460b6470c013a098e05
theantonioalmeida <tony-alm@hotmail.com>
2018-04-15 00:52:54 +0100

    Fix followers page layout

--- resources/views/partials/follow.blade.php
+++ resources/views/partials/follow.blade.php
@@ -1,18 +1,16 @@
 @auth
-<p class="align-self-center">
 @if(Auth::user()->id != $follower->id)
 @if(Auth::user()->isFollowing($follower))
     <form method="POST" action="{{Route('api.unFollow', $follower)}}">
         {{ method_field('DELETE') }}
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-sm btn-primary"><i class="far fa-user"></i> Following</button>
+        <button type="submit" class="btn btn-sm btn-primary"><i class="far fa-user"></i> <span class="text-collapse">Following</span></button>
     </form>
 @else
     <form method="POST" action="{{Route('api.follow', $follower)}}">
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-sm btn-outline-primary"><i class="far fa-fw fa-user-plus"></i> Follow</button>
+        <button type="submit" class="btn btn-sm btn-outline-primary"><i class="far fa-fw fa-user-plus"></i> <span class="text-collapse">Follow</span></button>
     </form>
 @endif
 @endif
-</p>
 @endauth
