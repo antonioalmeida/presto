5b5896693fee81c443af9d589d53d3fb2c1a791b
theantonioalmeida <tony-alm@hotmail.com>
2018-04-15 00:52:54 +0100

    Fix followers page layout

--- resources/views/pages/profile/followers.blade.php
+++ resources/views/pages/profile/followers.blade.php
@@ -11,35 +11,35 @@
 
     @include('pages.profile.profile')
 
-
     <!-- content -->
-        <section class="container my-4">
-            <h3>Followers</h3>
+    <section class="container">
 
-            <div class="card-deck mt-md-4">
-                @foreach($member->followers as $follower)
-                    @if($loop->index % 4 == 0 && !$loop->first && !$loop->last)
-            </div>
-            <div class="card-deck mt-md-4">
-                @endif
-                <div class="card">
-                    <div class="card-body d-flex flex-wrap justify-content-between">
-                        <div class="d-flex align-items-center">
-                            <div>
-                                <img class="user-preview rounded-circle pr-2" width="60px" heigth="60px" src="{{$follower->profile_picture}}">
-                            </div>
-                            <div>
-                                <a class="text-dark" href="{{Route('profile', $follower->username)}}"><h5>{{ $follower->name}}</h5></a>
-                                <h6><small class="text-muted"><i class="fas fa-gem text-primary"></i>{{$follower->score}} points</small></h6>
-                            </div>
-                        </div>
-                        <p class="text-collapse">{{$follower->bio}}</p>
-                        @include('partials.follow', ['follower' => $follower])
+        <div class="list-group offset-md-3 col-md-7">
+            <h4 class="mb-4">{{ $member->name }}'s followers</h4>
+            @foreach($member->followers as $follower)
+                <div onclick="location.assign('{{Route('profile', $follower->username)}}');" class="list-group-item list-group-item-action flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-begin">
+
+                    <div class="align-self-center">
+                       <img class="rounded-circle pr-2" width="60px" heigth="60px" src="{{$follower->profile_picture}}">
+                   </div>
+                    <div class="d-flex flex-column">
+                        <h6 class="mb-1">{{ $follower->name }}</h6>
+                        <h6 class="text-collapse"><small>{{ $follower->bio }}</small></h6>
+                        <h6><small class="text-muted"><i class="fas fa-gem text-primary"></i> {{$follower->score}} points</small></h6>
                     </div>
+
+                    <div class="ml-auto align-self-center flex-wrap">
+                    @include('partials.follow', ['follower' => $follower])
+                    </div>
+
+                  </div>
+
                 </div>
-                @endforeach
-            </div>
-        </section>
+            @endforeach
+        </div>
+    </section>
+
     </main>
 
 
