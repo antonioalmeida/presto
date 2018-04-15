866bd8b22969d7ef2c6069161743c2f2d67455ef
theantonioalmeida <tony-alm@hotmail.com>
2018-04-15 02:10:33 +0100

    Fix question card title size issue

--- resources/views/partials/question-card.blade.php
+++ resources/views/partials/question-card.blade.php
@@ -1,7 +1,9 @@
 <div onclick="location.assign('/questions/{{$question->id}}');" class="list-group-item list-group-item-action flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between flex-wrap-reverse">
-        <h4 class="mb-1">{{substr($question->title,0,38)}}{{(strlen($question->title)>39? '...' : '')}}</h4>
-        <small class="pb-1"><a href="{{Route('profile', $question->member->username)}}" class="btn-link"><img class="user-preview rounded-circle pr-1" width="36px" heigth="36px" src="{{$question->member->profile_picture}}">{{$question->member->name}}</a> <span class="text-muted">asked</span></small>
+    <div class="d-flex w-100 justify-content-between flex-md-nowrap flex-wrap-reverse">
+        <h4 class="mb-1 max-w-70">{{ $question->title }}</h4>
+        <div class="pb-1">
+            <small><a href="{{Route('profile', $question->member->username)}}" class="btn-link"><img class="user-preview rounded-circle pr-1" width="36px" heigth="36px" src="{{$question->member->profile_picture}}">{{$question->member->name}}</a> <span class="text-muted">asked</span></small>
+        </div>
     </div>
 
     <small class="text-muted"><i class="far fa-tags"></i>
