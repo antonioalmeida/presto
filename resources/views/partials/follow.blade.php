d576148586f8e82990e62fa2eefadd39a3cc16c7
theantonioalmeida <tony-alm@hotmail.com>
2018-04-15 01:01:48 +0100

    Generalize follow mini-form

--- resources/views/partials/follow.blade.php
+++ resources/views/partials/follow.blade.php
@@ -1,13 +1,13 @@
 @auth
-@if(Auth::user()->id != $follower->id)
-@if(Auth::user()->isFollowing($follower))
-    <form method="POST" action="{{Route('api.unFollow', $follower)}}">
+@if(Auth::user()->id != $followTarget->id)
+@if(Auth::user()->isFollowing($followTarget))
+    <form method="POST" action="{{Route('api.unFollow', $followTarget)}}">
         {{ method_field('DELETE') }}
         {{ csrf_field() }}
         <button type="submit" class="btn btn-sm btn-primary"><i class="far fa-user"></i> <span class="text-collapse">Following</span></button>
     </form>
 @else
-    <form method="POST" action="{{Route('api.follow', $follower)}}">
+    <form method="POST" action="{{Route('api.follow', $followTarget)}}">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-sm btn-outline-primary"><i class="far fa-fw fa-user-plus"></i> <span class="text-collapse">Follow</span></button>
     </form>
