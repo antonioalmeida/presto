7efbf343cbfdd342fcc93dfbc36d344a91f999ca
theantonioalmeida <tony-alm@hotmail.com>
2018-04-15 20:03:36 +0100

    Implement edit profile picture

--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -16,21 +16,19 @@
                             <img class="profile-pic img-fluid rounded-circle m-2" src="{{$member->profile_picture}}" />
                             <span class="fa-layers fa-fw fa-2x">
                                 <i class="fas fa-circle text-shadow" style="color:white"></i>
-                                <a href=""><i class="fa-inverse fa-fw fas fa-pencil-alt text-muted" data-fa-transform="shrink-8"></i></a>
+                                <a href="" data-toggle="modal" data-target="#editPicture"><i class="fa-inverse fa-fw fas fa-pencil-alt text-muted" data-fa-transform="shrink-8"></i></a>
                             </span>
                         </div>
 
+                        <div class="col-md-6 mobile-center text-shadow edit-profile">
                         <form method="POST" action="{{Route('profile.update', $member)}}">
                             {{ method_field('PUT') }}
                             {{ csrf_field() }}
-                            <div class="col-md-6 mobile-center text-shadow edit-profile">
                                 <input name="name" type="text" class="form-control input-h2" id="exampleFormControlInput1" value="{{$member->name}}">
                                 <input name="username" type="text" class="form-control input-h4 mt-2" id="exampleFormControlInput1" value="{{$member->username}}">
 
-                                <!-- bio -->
-                                <div class="bio mt-3">
+                                <div class="mt-3">
                                     <input name="bio" type="text" class="form-control input-h6 lead-adapt mt-2" id="exampleFormControlInput1" value="{{$member->bio}}">
-
 
                                     <div class="ml-1 mt-3">
                                         <button type="submit" class="btn btn-light">Save</button>
@@ -40,11 +38,10 @@
 
                                 </div>
 
-                            </div>
-
                             @include ('includes.errors')
 
                         </form>
+                    </div>
 
                     </div>
 
@@ -57,5 +54,28 @@
 
     <!-- /.container -->
     </body>
+
+    <!-- edit picture modal -->
+    <div class="modal fade" id="editPicture" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form method="POST" action="{{ Route('api.edit-profile-pic')}}">
+                    {{ csrf_field() }}
+                    <div class="modal-body">
+                        <div>
+                            <h6><label for="profile-pic-url">Change your photo</label></h6>
+                            <div class="input-group">
+                                <input type="text" name="profile-pic-url" class="form-control" placeholder="New URL" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 @endsection
