d3b5ebef64d8626616ed21d5274f766c26429852
Darksystem <diogo.rey@hotmail.com>
2018-04-17 19:40:18 +0100

    Updated some classes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,8 +13,6 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-      'App\Card' => 'App\Policies\CardPolicy',
-      'App\Item' => 'App\Policies\ItemPolicy'
     ];
 
     /**
