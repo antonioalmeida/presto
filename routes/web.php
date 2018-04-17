b775a05977a14e83bbc891bcd87c23fc7e61ee9c
Darksystem <diogo.rey@hotmail.com>
2018-04-18 00:39:11 +0100

    Implemented view answer

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('notifications', 'ProfileController@notifications')->name('notificati
 Route::get('admin', 'AdminController@show')->name('admin');
 
 // Answer
-Route::get('questions/{question_id}/answers/{answer_id}', 'AnswerController@show')->name('answer');
+Route::get('questions/{question}/answers/{answer}', 'AnswerController@show')->name('answer');
 
 // Question
 Route::get('questions/{question}', 'QuestionController@show')->name('question');
