3b9da4a9f2d9e6ecc2ca6d3fc2cfdb4dfc0089d0
darksystem <diogo.rey@hotmail.com>
2018-04-26 20:34:04 +0100

    Refactor gh-pages

--- lbaw-laravel/compile-assets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while true; do 
-  php vendor/bin/lessc -w resources/assets/less/app.less public/css/app.css
-done
--- lbaw-laravel/docker_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-env >> /var/www/.env
-php-fpm7.1 -D
-nginx -g "daemon off;"
