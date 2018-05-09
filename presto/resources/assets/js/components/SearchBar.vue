b8d122b694a93fe4cf993333cee2e1c9c0d771a0
António Almeida <theantonioalmeida@gmail.com>
2018-05-09 17:16:17 +0100

    Implement search input on navbar

---
+++ presto/resources/assets/js/components/SearchBar.vue
@@ -0,0 +1,53 @@
+<template>
+
+    <div class="input-group">
+        <a @click="onClick" id="nav-search-btn" class="nav-link" role="button"><i class="far fa-search"></i></a>
+
+        <b-form-input 
+            id="nav-search-bar"
+            type="text"
+            v-model="query"
+            placeholder="Search..."
+            :state="showError ? false : null"
+            @change="onSubmit"
+            @input="showError = false"
+            :class="{'open' : isOpen}"
+            >
+        </b-form-input>
+        </div>
+
+</template>
+
+<script>
+export default {
+
+    name: 'SearchBar',
+
+    data () {
+        return {
+            query: '',
+            showError: false,
+            isOpen: false
+        }
+    },
+
+    methods: {
+        onClick: function() {
+            this.isOpen = !this.isOpen;
+
+            if(this.query.length === 0)
+                this.showError = true;
+        },
+
+        onSubmit: function() {
+            if(this.query.length === 0) {
+                this.showError = true;
+                return;
+            }
+
+            this.$router.push({ path: '/search/' + this.query });
+        }
+    }
+}
+</script>
+
