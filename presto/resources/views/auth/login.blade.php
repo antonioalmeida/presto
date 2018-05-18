d91a424ac634d53e2e3bff77a27bcf3f3112e62e
darksystem <diogo.rey@hotmail.com>
2018-05-18 10:15:29 +0100

    Implemented google login api

--- presto/resources/views/auth/login.blade.php
+++ presto/resources/views/auth/login.blade.php
@@ -44,6 +44,26 @@
             <div class="m-2 g-signin2" data-width="254" data-height="40" data-longtitle="true"></div>
             <div class="fb-login-button m-2" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
           </div>
+
+          <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+<!--Other form fields above the button-->
+    <div class="form-group">
+        <div class="col-md-6 col-md-offset-4">
+            <button type="submit" class="btn btn-primary">
+            <i class="fa fa-btn fa-sign-in"></i> Login
+            </button>
+        </div>
+    </div>
+    <hr>
+    <div class="form-group">
+        <div class="col-md-6 col-md-offset-4">
+            <a href="{{ url('/auth/google') }}" class="btn btn-google"><i class="fa fa-google"></i> Github</a>
+            <a href="{{ url('/auth/twitter') }}" class="btn btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
+            <a href="{{ url('/auth/facebook') }}" class="btn btn-facebook"><i class="fa fa-facebook"></i> Facebook</a>
+        </div>
+    </div>
+</form>
+
         </div>
       </div>
     </section>
