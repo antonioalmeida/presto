4e7d7fb7ec8fc09ccdbbd89041918ef54f77efd1
António Almeida <theantonioalmeida@gmail.com>
2018-05-25 01:10:04 +0100

    Improve mentions UI and UX - needs some tweaks

--- presto/resources/assets/js/components/CommentBox.vue
+++ presto/resources/assets/js/components/CommentBox.vue
@@ -2,88 +2,91 @@
   <div class="card-footer">
     <vue-tribute :options="options">
       <b-form-textarea
-              v-model="commentText"
-              placeholder="Leave a comment..."
-              :rows="2"
-              :max-rows="6"
-              :state="showError ? !showError : null" @input="showError = false; showSuccess = false;">
-      </b-form-textarea>
-    </vue-tribute>
+      v-model="commentText"
+      placeholder="Leave a comment..."
+      :rows="2"
+      :max-rows="6"
+      :state="showError ? !showError : null" @input="showError = false; showSuccess = false;">
+  </b-form-textarea>
+</vue-tribute>
 
-      <span v-if="showError" class="text-danger"><small>You can't submit an empty comment.</small></span>
-      <span v-if="showSuccess" class="text-primary"><small>Comment successfully added.</small></span>
+<span v-if="showError" class="text-danger"><small>You can't submit an empty comment.</small></span>
+<span v-if="showSuccess" class="text-primary"><small>Comment successfully added.</small></span>
 
 
-      <div class="mt-2">
-          <button @click="onCommentSubmit" class="btn btn-sm btn-primary">Submit</button>
-          <button v-if="this.parentType=='question'" v-b-toggle.accordion2 class="btn btn-sm btn-link">Cancel</button>
-      </div>
-  </div>
+<div class="mt-2">
+    <button @click="onCommentSubmit" class="btn btn-sm btn-primary">Submit</button>
+    <button v-if="this.parentType=='question'" v-b-toggle.accordion2 class="btn btn-sm btn-link">Cancel</button>
+</div>
+</div>
 </template>
 
 <script>
-    import VueTribute from 'vue-tribute';
+  import VueTribute from 'vue-tribute';
 
-    export default {
+  export default {
 
-        props: ['parentType', 'parent'],
+    props: ['parentType', 'parent'],
 
-        name: 'CommentBox',
+    name: 'CommentBox',
 
-        components: {
-          'VueTribute': VueTribute
-        },
+    components: {
+      'VueTribute': VueTribute
+  },
 
-        data() {
-            return {
-                commentText: '',
-                showError: false,
-                showSuccess: false,
+  data() {
+      return {
+        commentText: '',
+        showError: false,
+        showSuccess: false,
 
                 //vue-tribute mentions options
                 options: {
                   menuItemTemplate: function (item) {
-                   return '<img style="width:10%;height:10%;" alt="'+item.string+'\'s profile picture" src="'+item.original.profile_picture + '">' + item.string;
-                  },
+                   //return '<img style="width:10%;height:10%;" alt="" src="'+item.original.profile_picture + '">' + item.string;
+                   return item.string;
+               },
+               /*
                   noMatchTemplate: function() {
-                    return 'No members found!';
+                    return '<small>No match</small>';
                   },
+                  */
                   values: function (text, cb) {
-                  axios.get('/api/search/'+text, {
+                    axios.get('/api/search/'+text, {
                       params: {
-                          type: 'members'
-                      }
+                        type: 'members'
+                    }
+                })
+                    .then(({data}) => {
+                      cb(data);
                   })
-                      .then(({data}) => {
-                        cb(data);
-                      })
-                      .catch((error) => {
-                        cb([]);
-                      });
-                  },
-                lookup: 'username',
+                    .catch((error) => {
+                      cb([]);
+                  });
+                },
+                lookup: 'name',
                 fillAttr: 'username'
-                }
             }
-        },
+        }
+    },
 
-        methods: {
-            append: function() {
-              let kv = Math.random()
-                .toString(36)
-                .slice(2)
-              this.options.values.push({
-                key: kv,
-                value: kv
-              })
-            },
-            onCommentSubmit: function () {
-                if (this.commentText == '') {
-                    this.showError = true;
-                    return;
-                }
-                else
-                    this.showError = false;
+    methods: {
+      append: function() {
+        let kv = Math.random()
+        .toString(36)
+        .slice(2)
+        this.options.values.push({
+          key: kv,
+          value: kv
+      })
+    },
+    onCommentSubmit: function () {
+        if (this.commentText == '') {
+          this.showError = true;
+          return;
+      }
+      else
+          this.showError = false;
 
                //Extract mentions from comment
                let currentMention;
@@ -93,20 +96,21 @@
                while ((currentMention = regex.exec(this.commentText)) !== null)
                  mentions.push(currentMention[1]);
 
-                axios.post('/api/comments/' + this.parentType + '/' + this.parent.id, {
-                    'content': this.commentText,
-                    'mentions': mentions
-                })
-                    .then(({data}) => {
-                        this.parent.comments.unshift(data);
-                        this.commentText = '';
-                        this.showSuccess = true;
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-            }
-        }
+             axios.post('/api/comments/' + this.parentType + '/' + this.parent.id, {
+                'content': this.commentText,
+                'mentions': mentions
+            })
+             .then(({data}) => {
+                this.parent.comments.unshift(data);
+                this.commentText = '';
+                this.showSuccess = true;
+            })
+             .catch((error) => {
+                console.log(error);
+            });
+         }
+     }
 
-    }
+ }
 </script>
+
