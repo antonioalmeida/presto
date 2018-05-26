833a0b48e27c0d9664b257da67e796273240985b
darksystem <diogo.rey@hotmail.com>
2018-05-26 22:07:11 +0100

    Fix image on production and added inserts on notifications

--- presto/docker_run.sh
+++ presto/docker_run.sh
@@ -4,6 +4,5 @@ set -e
 env >> /var/www/.env
 php-fpm7.1 -D
 cd /var/www
-php artisan migrate:fresh --force
-php artisan db:seed --force
+php artisan migrate:fresh --seed --force
 nginx -g "daemon off;"
