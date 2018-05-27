87e00a3f27adcc30a45a004d83e0a89f62bed9ba
darksystem <diogo.rey@hotmail.com>
2018-05-27 12:01:33 +0100

    Fix notifications cards

--- presto/resources/assets/js/components/NotificationsCard.vue
+++ presto/resources/assets/js/components/NotificationsCard.vue
@@ -5,48 +5,48 @@
         <div class="notifications-card d-flex w-100 justify-content-between flex-wrap">
             <div>
                 <template v-if="notification.data['type'] == 'Follow'">
-                    <a :href="'/profile/' + notification.data['follower_username']" class="pb-1">
+                    <router-link :to="'/profile/' + notification.data['follower_username']" class="pb-1">
                         <img class="rounded-circle pr-1" width="36" height="36"
-                             :src="notification.data['follower_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['follower_name']}}</a>
+                             :src="notification.data['follower_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['follower_name']}}</router-link>
                     <span class="text-muted"> started following you.</span>
                 </template>
 
                 <template v-if="notification.data['type'] == 'Question'">
-                    <a :href="'/profile/' + notification.data['following_username']" class="pb-1">
+                    <router-link :to="'/profile/' + notification.data['following_username']" class="pb-1">
                         <img class="rounded-circle pr-1" width="36" height="36"
-                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</a>
+                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</router-link>
                     <span class="text-muted"> posted a question.</span>
                 </template>
 
                 <template v-if="notification.data['type'] == 'Answer'">
-                    <a :href="'/profile/' + notification.data['following_username']" class="pb-1">
+                    <router-link :to="'/profile/' + notification.data['following_username']" class="pb-1">
                         <img class="rounded-circle pr-1" width="36" height="36"
-                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</a>
+                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</router-link>
                     <span class="text-muted"> answered your question: </span> {{notification.data['question_title']}}
                 </template>
 
                 <template v-if="notification.data['type'] == 'Comment'">
-                    <a :href="'/profile/' + notification.data['following_username']" class="pb-1">
+                    <router-link :to="'/profile/' + notification.data['following_username']" class="pb-1">
                         <img class="rounded-circle pr-1" width="36" height="36"
-                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</a>
+                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</router-link>
                     <span class="text-muted"> left a comment on your<span
                             v-if="notification.data['type_comment'] == 'Answer' "> answer to the </span> question: </span><a
                         :href="'/questions/' + notification.data['question_id']" class="pb-1">{{notification.data['question_title']}}</a>
                 </template>
 
                  <template v-if="notification.data['type'] == 'Rating'">
-                    <a :href="'/profile/' + notification.data['following_username']" class="pb-1">
+                    <router-link :to="'/profile/' + notification.data['following_username']" class="pb-1">
                         <img class="rounded-circle pr-1" width="36" height="36"
-                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</a>
+                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</router-link>
                     <span class="text-muted"> upvote your comment to the<span
                             v-if="notification.data['type_comment'] == 'Answer' "> answer to the </span> question: </span><a
                         :href="'/questions/' + notification.data['question_id']" class="pb-1">{{notification.data['question_title']}}</a>
                 </template>
 
                  <template v-if="notification.data['type'] == 'Mention'">
-                    <a :href="'/profile/' + notification.data['following_username']" class="pb-1">
+                    <router-link :to="'/profile/' + notification.data['following_username']" class="pb-1">
                         <img class="rounded-circle pr-1" width="36" height="36"
-                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</a>
+                             :src="notification.data['following_picture']" :alt="notification.data['follower_name']+'\'s profile picture'">{{notification.data['following_name']}}</router-link>
                     <span class="text-muted"> mentioned you on their comment to the <span
                             v-if="notification.data['type_comment'] == 'Answer' "> answer to the </span> question: </span><a
                         :href="'/questions/' + notification.data['question_id']" class="pb-1">{{notification.data['question_title']}}</a>
