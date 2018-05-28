77e3292beeab455976d27334c7076166159f15a0
Bruno Piedade <dako1oblivion@hotmail.com>
2018-05-28 19:16:02 +0100

    Working notifications pagination

--- presto/app/Utils.php
+++ presto/app/Utils.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+
 function print_number_count($number)
 {
     $precision = 0;
@@ -29,3 +33,20 @@ function print_number_count($number)
     return number_format($number / $divisor, $precision) . $shorthand;
 
 }
+
+ /**
+    * Gera a paginação dos itens de um array ou collection.
+    *
+    * @param array|Collection      $items
+    * @param int   $perPage
+    * @param int  $page
+    * @param array $options
+    *
+    * @return LengthAwarePaginator
+    */
+    function paginate($items, $perPage = 15, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
