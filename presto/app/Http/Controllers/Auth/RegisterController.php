cc3d54a6f97c0e89fa3916944a9bd564a2a793fb
Cyril Damas <jpdamas15@gmail.com>
2018-05-28 12:27:25 +0100

    Add default dummy image for members and topics

--- presto/app/Http/Controllers/Auth/RegisterController.php
+++ presto/app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,7 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'profile_picture' => 'https://dummyimage.com/250/11214b/ffffff.png&text=' . $data['username']
         ]);
 
     }
