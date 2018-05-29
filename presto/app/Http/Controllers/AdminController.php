c78b7148e984e62d976d0d84c0c0297328bda554
darksystem <diogo.rey@hotmail.com>
2018-05-29 01:05:06 +0100

    Deleted old blades and deleted show function on admin

--- presto/app/Http/Controllers/AdminController.php
+++ presto/app/Http/Controllers/AdminController.php
@@ -75,17 +75,4 @@ class AdminController extends Controller
         $flag = Flag::where(['member_id' => $member_id, 'moderator_id' => $moderator_id])->first();
         $flag->delete();
     }
-
-    public function show()
-    {
-        $members = Member::get();
-        $flagged = Flag::orderBy('date')->get();
-        $banned = Member::where('is_banned', true)->get();
-        $moderators = Member::where('is_moderator', true)->get();
-        $certified = Member::where('is_certified', true)->get();
-
-        $members = Member::paginate(10);
-
-        return view('pages.admin', ['members' => $members, 'flagged' => $flagged, 'banned' => $banned, 'moderators' => $moderators, 'certified' => $certified]);
-    }
 }
